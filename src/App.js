87bc628f099fea29d777cc813442b199be7cf39c
Mahmut <makhmud.kochkanov@gmail.com>
2022-04-19 13:07:21 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-
-
-import {Button} from './component/Button'
-import {Count} from './component/count/Count'
-import './App.css';
-import { useState } from 'react';
+import { Button } from "./component/Button";
+import { Count } from "./component/count/Count";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count,setCount] = useState(1)
-  return (
-    <div className="App">
-      <Count counter={count}/>
-     <Button setCount={setCount}title='+'/>
-     <Button setCount={setCount} title='-'/>
-    
-    </div>
-  );
+  const [count, setCount] = useState(1);
+  return <div className="App"></div>;
 }
 
 export default App;
