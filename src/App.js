200fc645278f31a188d4c50c75bc49b10c85cef8
Mahmut <makhmud.kochkanov@gmail.com>
2022-04-19 13:09:24 +0300

    example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { Button } from "./component/Button";
-import { Count } from "./component/count/Count";
-import "./App.css";
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(1);
   return <div className="App"></div>;
 }
 
--- src/component/count/Count.jsx
+++
@@ -1,7 +0,0 @@
-export const Count=(props)=>{
-    let counter = props.counter
-
-    return(
-        <h2>{counter}</h2>
-    )
-}
